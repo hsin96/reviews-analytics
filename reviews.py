bd1ec77dd3c2f747f83040b61187f4c75ee24562
YiHsin Chen <hsin9696.study@gmail..com>
2022-09-14 17:06:48 -0400

    average lenght

--- reviews.py
+++ reviews.py
@@ -4,9 +4,18 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 #count = count + 1
-		if count % 1000 == 0:
+		if count % 1000 == 0: #只印出1000的倍數
 			print(len(data))
-print(len(data))
+print('檔案讀取完了總共有', len(data), '筆資料')
+
+#算留言平均長度
+
+sum_len = 0
+for d in data: #在data的每一筆資料命名為d
+	sum_len += len(d)
+print('留言的平均長度為', sum_len/len(data))
+
+
 print(data[0])
 print('.....................')
 print(data[2])
